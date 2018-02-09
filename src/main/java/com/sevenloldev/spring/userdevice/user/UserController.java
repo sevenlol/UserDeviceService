1f9d4534332509ee5bc9ee9a4d1ffc1d5fb90b47
sevenlol <sevenlol1007@gmail.com>
2018-02-09 16:03:30 +0800

    Implement basic User CRUD APIs
    1. create user
    2. query users
    3. get user by id
    4. update user
    5. delete user

---
+++ src/main/java/com/sevenloldev/spring/userdevice/user/UserController.java
@@ -0,0 +1,69 @@
+package com.sevenloldev.spring.userdevice.user;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+  @Autowired
+  private UserRepository repo;
+
+  @PostMapping("/users")
+  public Map<String, String> createUser(
+      @Valid @RequestBody User user, BindingResult result) {
+    check(result);
+    return getUserIdResponse(repo.create(user));
+  }
+
+  @GetMapping("/users")
+  public UserQueryResult queryUsers(
+      @Valid UserQueryRequest req, BindingResult result) {
+    check(result);
+    return repo.query(new UserQuery(req));
+  }
+
+  @GetMapping("/users/{id}")
+  public User getUserById(@PathVariable("id") String id) {
+    return repo.get(id);
+  }
+
+  @PutMapping("/users/{id}")
+  public Map<String, String> updateUser(
+      @PathVariable("id") String id,
+      @Valid @RequestBody User user, BindingResult result) {
+    check(result);
+    repo.update(id, user);
+    return getUserIdResponse(id);
+  }
+
+  // TODO partial update
+
+  @DeleteMapping("/users/{id}")
+  public Map<String, String> deleteUser(
+      @PathVariable("id") String id) {
+    repo.delete(id);
+    return getUserIdResponse(id);
+  }
+
+  private void check(BindingResult result) {
+    if (result.hasErrors()) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  private Map<String, String> getUserIdResponse(String id) {
+    Map<String, String> response = new HashMap<>();
+    response.put("user_id", id);
+    return response;
+  }
+}
