4b8c1c702f6f63bd48e555c1132949f07668ad1f
sevenlol <sevenlol1007@gmail.com>
2018-02-12 17:08:35 +0800

    Implement query method in DataType repository
    1. Validate query
    2. Create JPA specification with DeviceTypeQuery
    3. Query DeviceType entities with pagination / filtering and sorting

--- src/main/java/com/sevenloldev/spring/userdevice/device/type/SpringDataDeviceTypeRepository.java
+++ src/main/java/com/sevenloldev/spring/userdevice/device/type/SpringDataDeviceTypeRepository.java
@@ -3,9 +3,26 @@ package com.sevenloldev.spring.userdevice.device.type;
 import com.sevenloldev.spring.userdevice.util.error.ResourceNotExistException;
 import com.sevenloldev.spring.userdevice.util.error.ServerErrorException;
 import com.sevenloldev.spring.userdevice.util.response.QueryResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,7 +48,25 @@ public class SpringDataDeviceTypeRepository implements DeviceTypeRepository {
 
   @Override
   public QueryResponse<DeviceType> query(DeviceTypeQuery query) {
-    return null;
+    Specification<DeviceType> spec = getSpec(query);
+
+    int page = query.getOffset() / query.getLimit();
+    Pageable pageable = PageRequest.of(page, query.getLimit());
+    try {
+      Page<DeviceType> deviceTypes = repo.findAll(spec, pageable);
+      List<DeviceType> result = new ArrayList<>();
+      for (DeviceType type : deviceTypes) {
+        result.add(type);
+      }
+      QueryResponse<DeviceType> response = new QueryResponse<>(
+          (int) deviceTypes.getTotalElements(),
+          result
+      );
+      return response;
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new ServerErrorException(e);
+    }
   }
 
   @Override
@@ -138,5 +173,65 @@ public class SpringDataDeviceTypeRepository implements DeviceTypeRepository {
   private int getType(String type) {
     checkType(type);
     return Integer.parseInt(type);
+  }
+
+  private Specification<DeviceType> getSpec(DeviceTypeQuery query) {
+    check(query);
+    return new DeviceTypeSpec(query);
+  }
+
+  private void check(DeviceTypeQuery query) {
+    checkNotNull(query);
+    // NOTE trying out validator, not using DI
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
+    Set<ConstraintViolation<DeviceTypeQuery>> result =  validator.validate(query);
+    checkNotNull(result);
+    if (!result.isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+    // TODO use hibernate validator cross field validation
+    // offset is multiple of limit
+    checkArgument(query.getOffset() % query.getLimit() == 0);
+  }
+
+  private class DeviceTypeSpec implements Specification<DeviceType> {
+    private final DeviceTypeQuery query;
+
+    public DeviceTypeSpec(DeviceTypeQuery query) {
+      check(query);
+      this.query = query;
+    }
+
+    @Nullable
+    @Override
+    public Predicate toPredicate(Root<DeviceType> root, CriteriaQuery<?> query,
+        CriteriaBuilder cb) {
+      List<Predicate> predicates = new ArrayList<>();
+      // configure filtering predicate
+      if (this.query.getName() != null) {
+        predicates.add(cb.equal(root.get("name"), this.query.getName()));
+      }
+      if (this.query.getModelname() != null) {
+        predicates.add(cb.equal(root.get("modelname"), this.query.getModelname()));
+      }
+      if (this.query.getManufacturer() != null) {
+        predicates.add(cb.equal(root.get("manufacturer"), this.query.getManufacturer()));
+      }
+
+      // configure order by (in criteria query)
+      String sort = this.query.getSort();
+      boolean asc = true;
+      if (sort.startsWith("-")) {
+        asc = false;
+        sort = sort.substring(1);
+      }
+      if (asc) {
+        query.orderBy(cb.asc(root.get(sort)));
+      } else {
+        query.orderBy(cb.desc(root.get(sort)));
+      }
+      return cb.and(predicates.toArray(new Predicate[] {}));
+    }
   }
 }
